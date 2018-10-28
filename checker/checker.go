9d8bed61c116128a576a63a3f6ca2fb346f620c2
petarj <petarj@protonmail.ch>
2018-10-28 20:33:43 +0100

    initial commit

---
+++ checker/checker.go
@@ -0,0 +1,49 @@
+package checker
+
+import (
+	"crypto/md5"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+type Checker interface {
+	List()
+	Get()
+	Compare()
+}
+
+type FileChecker struct {
+}
+
+type HashRes struct {
+	FileName string
+	Hash     string
+}
+
+// List directories and files with their hashes.
+func (fc FileChecker) List(root string, skips map[string]bool) (hr []HashRes, err error) {
+	// TODO mora postojati skip dir lista
+	err = filepath.Walk(root, func(path string, info os.FileInfo, err0 error) error {
+		if info.IsDir() {
+			if skips[path] {
+				return filepath.SkipDir
+			}
+			hr = append(hr, HashRes{FileName: path})
+		} else {
+			f, err2 := os.Open(path)
+			if err2 != nil {
+				return err2
+			}
+			defer f.Close()
+			h := md5.New()
+			if _, err3 := io.Copy(h, f); err3 != nil {
+				log.Fatal(err3)
+			}
+			hr = append(hr, HashRes{FileName: path, Hash: string(h.Sum(nil))})
+		}
+		return nil
+	})
+	return hr, err
+}
---
+++ drift.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"drift/checker"
+
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var (
+	fc checker.FileChecker
+)
+
+func main() {
+	fmt.Println("vim-go")
+
+	isServer := flag.Bool("d", false, "Run as daemon.")
+	flag.Parse()
+
+	if *isServer {
+		router := mux.NewRouter()
+		router.HandleFunc("/checkers/FileChecker", getFileChecker).Methods("GET")
+		log.Fatal(http.ListenAndServe(":8000", router))
+	} else {
+		// TODO get server addresses and passwords
+		// TODO connect and gather info from checkers
+		// TODO calculate differences
+		// TODO show diffs...
+	}
+}
+
+func startServer() {
+
+}
+
+func startClient() {
+	// TODO get server addresses and passwords
+	// TODO connect and gather info from checkers
+	// TODO calculate differences
+	// TODO show diffs...
+}
+
+func getFileChecker(w http.ResponseWriter, r *http.Request) {
+	// TODO fill exclusion map from request params
+	skips := make(map[string]bool)
+	hrs, err := fc.List("/", skips)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, err := json.Marshal(hrs)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+}
