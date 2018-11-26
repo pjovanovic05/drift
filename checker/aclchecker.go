3e2e6454d00b189078fb8e4f6b211c583d3d26d1
Petar Jovanovic <petarj@ipb.ac.rs>
2018-11-27 00:28:24 +0100

    acl mic

--- checker/aclchecker.go
+++ checker/aclchecker.go
@@ -1,6 +1,11 @@
 package checker
 
-import "sync"
+import (
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+)
 
 type ACLChecker struct {
 	BasicChecker
@@ -9,6 +14,33 @@ type ACLChecker struct {
 
 func (aclc *ACLChecker) Collect(config map[string]string) {
 	// TODO:
+	aclc.mu.Lock()
+	aclc.collected = aclc.collected[:0]
+	aclc.mu.Unlock()
+	skipPaths := strings.Split(config["skips"], ":")
+	targetPath := config["path"]
+
+	// create skip map
+	skips := make(map[string]bool)
+	for _, dir := range skipPaths {
+		skips[dir] = true
+	}
+
+	aclc.err = filepath.Walk(targetPath, func(path string, info os.FileInfo, err0 error) error {
+		if skips[path] {
+			if info.IsDir() {
+				return filepath.SkipDir
+			}
+			return nil
+		}
+		aclc.mu.Lock()
+		B
+		aclc.collected = append(aclc.collected, Pair{Key: path, Value: info.Mode().String()})
+		aclc.progress = path
+		aclc.mu.Unlock()
+		// TODO: get owner uid and gid:
+		// https://groups.google.com/forum/#!topic/golang-nuts/ywS7xQYJkHY
+	})
 }
 
 func (aclc *ACLChecker) Progress() string {
