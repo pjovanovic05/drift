db5e654bbf159d989883f5601f73ff6da4ff413c
Petar Jovanovic <petarj@ipb.ac.rs>
2018-11-28 00:01:56 +0100

    mic

--- drift.go
+++ drift.go
@@ -8,6 +8,8 @@ func main() {
 	isServer := flag.Bool("d", false, "Run as daemon.")
 	runConfig := flag.String("config", "run-config.json",
 		"JSON config of targets to compare.")
+	askPass := flag.Bool("p", false, "Ask for password.")
+	passwd := flag.String("pass", "", "Password for clients to connect.")
 	reportFN := flag.String("o", "drift-report.html",
 		"File name for the report to be generated.")
 	flag.Parse()
@@ -18,3 +20,7 @@ func main() {
 		startClient(*runConfig, *reportFN)
 	}
 }
+
+// TODO: get password: https://stackoverflow.com/questions/2137357/getpasswd-functionality-in-go
+// TODO: make https connections
+// TODO: make http basic auth
